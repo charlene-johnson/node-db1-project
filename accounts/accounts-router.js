3c34b004e44d25c39b3ff01d868888cf35a78d3a
charlene-johnson <charlene.j8234@gmail.com>
2020-08-19 20:54:08 -0400

    Started writing endpoints, did not write logic yet

---
+++ accounts/accounts-router.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const db = require("../data/dbConfig")
+
+const router = express.Router()
+
+router.get("/", async (req, res, next) => {
+
+})
+
+router.get("/:id", async (req, res, next) => {
+
+})
+
+router.post("/", async (req, res, next) => {
+
+})
+
+router.put("/:id", async (req, res, next) => {
+
+})
+
+router.delete("/:id", async (req, res, next) => {
+
+})
+
+
+module.exports = router
